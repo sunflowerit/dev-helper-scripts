20bf051550162c475f978ae0fb546bd956a9754f
hayyan <hayyan-ebrahem@hotmail.com>
2018-04-22 22:15:46 +0200

    first push of the dev helper package

---
+++ setup.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+if sys.version_info < (2, 6):
+    sys.stderr.write("This package requires Python 2.6 or newer. "
+                     "Yours is " + sys.version + os.linesep)
+    sys.exit(1)
+
+# a sufficient version of pip is needed to parse Odoo requirement file
+# version 1.4.1 is the one required by reportlab anyway
+requires = ['anybox.recipe.odoo']
+
+if sys.version_info < (2, 7):
+    requires.append('ordereddict')
+    requires.append('argparse')
+
+setup(
+    name="dev.helper.scripts",
+    version=version,
+    author="Sunflower IT",
+    author_email="info@sunflowerweb.nl",
+    description="Buildout scripts fto be used internally by the developer to work with Odoo",
+    license="AGPLv3+",
+    url="https://github.com/sunflowerit",
+    packages=find_packages(),
+    namespace_packages=['dev', 'dev.helper'],
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=requires,
+    tests_require=requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Buildout :: Recipe',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or '
+        'later (AGPLv3+)',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    scripts=['dev/helper/scripts/psql-odoo'],
+
+)
